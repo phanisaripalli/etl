d714bc4193463f90eafb365901968a06824705c1
Phani <saripalli.phani@gmail.com>
2017-08-04 19:48:54 +0200

    handler func

--- handler.py
+++ handler.py
@@ -16,7 +16,7 @@ def excutor(event, context):
    
     client = boto3.client('stepfunctions')
     try:
-        response = client.start_execution(stateMachineArn='arn:aws:states:eu-central-1:876417061267:stateMachine:ETLStepMachine',name='ETLStepMachine-' +now, input = json.dumps(event))
+        response = client.start_execution(stateMachineArn='xxxxxxxxxxxxx',name='ETLStepMachine-' +now, input = json.dumps(event))
     except:        
         print (sys.exc_info()[0])
         return False
